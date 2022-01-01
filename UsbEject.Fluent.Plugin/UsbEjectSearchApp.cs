c63c674c7cb8162cbc337ecb7cc92a2a0cbec6a0
Makesh Vineeth <makeshvineeth1997@gmail.com>
2022-01-01 23:05:09 +0530

    Added initial files

---
+++ UsbEject.Fluent.Plugin/UsbEjectSearchApp.cs
@@ -0,0 +1,22 @@
+﻿using Blast.Core.Interfaces;
+using Blast.Core.Objects;
+
+namespace UsbEject.Fluent.Plugin;
+
+public class UsbEjectSearchApp: ISearchApplication
+{
+    public ValueTask<IHandleResult> HandleSearchResult(ISearchResult searchResult)
+    {
+        throw new NotImplementedException();
+    }
+
+    public SearchApplicationInfo GetApplicationInfo()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IAsyncEnumerable<ISearchResult> SearchAsync(SearchRequest searchRequest, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ UsbEject.Fluent.Plugin/UsbEjectSearchResult.cs
@@ -0,0 +1,10 @@
+﻿using Blast.Core.Results;
+
+namespace UsbEject.Fluent.Plugin;
+public class UsbEjectSearchResult : SearchResultBase
+{
+    protected override void OnSelectedSearchResultChanged()
+    {
+        throw new NotImplementedException();
+    }
+}
